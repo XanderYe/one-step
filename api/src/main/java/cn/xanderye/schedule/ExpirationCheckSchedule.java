c5f35529756329c0146f90893130327c103b1f7a
XanderYe <XanderYe@outlook.com>
2020-05-22 08:46:38 +0800

    修复定时bug

--- api/src/main/java/cn/xanderye/schedule/ExpirationCheckSchedule.java
+++ api/src/main/java/cn/xanderye/schedule/ExpirationCheckSchedule.java
@@ -20,12 +20,13 @@ public class ExpirationCheckSchedule {
     @Autowired
     private ActivityMapper activityMapper;
 
-    @Scheduled(cron = "0 0 5 * * ? *")
+    @Scheduled(cron = "0 0 5 * * ?")
     public void check() {
         log.info("开始清理过期活动");
         List<Activity> activityList = activityMapper.getActivities();
         for (Activity activity:activityList) {
             if (activity.getExpirationTime() != null && activity.getExpirationTime().getTime() > System.currentTimeMillis()) {
+                log.info("清理" + activity.getName());
                 activityMapper.expire(activity.getId());
             }
         }
